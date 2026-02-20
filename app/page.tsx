4e922937e4fe2e924e3bf5bf6fbcd35ba8e77404
ShikharS <shikhar0895@gmail.com>
2026-02-20 17:07:00 +0530

    added build and push action

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
+import React from "react";
+
+function GridContainer({ children}: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen min-w-screen relative">
+      <div className="h-full w-0.5 bg-gray-400 absolute z-20 top-0 left-[40px]  opacity-40  "></div>
+      <div className="h-full w-0.5 bg-gray-400 absolute z-20 top-0 right-[40px]  opacity-40 "></div>
+      <div className="h-0.5 w-screen bg-gray-400 absolute z-20 top-10 opacity-40 "></div>
+      <div className="w-full h-full absolute px-10 pt-10">
+        {children}
+      </div>
+
+    </div>
+  );
+}
+
 export default function Home() {
   return (
-    <div className="p-4">
-      Welcome to portfolio demo
-      Nothing to look here
-    </div>
+    <GridContainer>
+      <div className="max-w-3xl mx-auto pt-10 text-center ">
+        Welcome to portfolio website
+      </div>
+
+    </GridContainer>
+
   );
 }
